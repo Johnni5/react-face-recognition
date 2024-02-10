952b6ed7efe4a0c631956a4292fd38cad65afc50
johnni5 <johnni5@proton.me>
2024-02-10 19:25:34 +0100

    "clarifai API implementation - frontend 90% ready"

--- src/App.js
+++ src/App.js
@@ -4,35 +4,164 @@ import Logo from './components/logo/logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg';
+import React, { Component } from 'react';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 
-function App() {
-  return (
-    <div className="App">
-      <ParticlesBg
-        type="cobweb"
-        color="#ff0000"
-        num={200}
-        bg={true}
-        // "color"
-        // "ball"
-        // "lines"
-        // "thick"
-        // "circle"
-        // "cobweb"
-        // "polygon"
-        // "square"
-        // "tadpole"
-        // "fountain"
-        // "random"
-        // "custom"
-      />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* {<FaceRecognition />} */}
-    </div>
-  );
+// [![face-detection](https://clarifai.com/api/clarifai/main/models/face-detection/badge)](https://clarifai.com/clarifai/main/models/face-detection?tab=overview)
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+    };
+  }
+
+  displayFaceBox = box => {
+    console.log('my box data: ', box);
+    this.setState({ box: box });
+  };
+
+  calculateFaceLocation = region => {
+    const clarifaiFace =
+      region.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log('IMG w + h ', width, height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
+
+  onInputChange = event => {
+    console.log(event.target.value);
+    this.setState({ input: event.target.value });
+  };
+
+  onButtonSubmit = () => {
+    console.log('click');
+    this.setState({ imageUrl: this.state.input });
+    console.log('URL via onButtonSubmit-event: ', this.state.input);
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+    // In this section, we set the user authentication, user and app ID, model details, and the URL
+    // of the image we want as an input. Change these strings to run your own example.
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Your PAT (Personal Access Token) can be found in the portal under Authentification
+    // bfed1b17d97e43e5bd3f587f2ff57c0a
+    // 6dc7e46bc9124c5c8824be4822abe105
+    const PAT = 'bfed1b17d97e43e5bd3f587f2ff57c0a';
+    // Specify the correct user_id/app_id pairings
+    // Since you're making inferences outside your app's scope
+    const USER_ID = 'lurian06';
+    const APP_ID = 'my-first-application-jy170q';
+    // Change these to whatever model and image URL you want to use
+    const MODEL_ID = 'face-detection';
+    //const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
+    let IMAGE_URL = this.state.input;
+    console.log('URL via app.code: ', IMAGE_URL);
+    // To use image bytes, assign its variable
+    // const IMAGE_BYTES_STRING = '/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAUDBAQEAwUEBAQFBQUGBwwIBwcHBw8LCwkMEQ8SEhEPERETFhwXExQaFRERGCEYGh0dHx8fExciJCIeJBweHx7/2wBDAQUFBQcGBw4ICA4eFBEUHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh7/wAARCAAoACgDASIAAhEBAxEB/8QAGQAAAgMBAAAAAAAAAAAAAAAAAAYDBQcE/8QAMBAAAQMDAwMDAgQHAAAAAAAAAQIDBAAFEQYSIQcTMTJBURRhCBYikSNScXKhsdH/xAAZAQACAwEAAAAAAAAAAAAAAAAFBgIDBAf/xAAtEQABAwMBBgQHAQAAAAAAAAABAgMRAAQhMQUSE0FRYQaBocEUFiJCcrHR8P/aAAwDAQACEQMRAD8A3+RYY1unSYzCS0ttZUkAgktn0q5yT7jPyDUC4wdGwycH5U2Kt9ZQ7VI1qw5PkvQy3CSVPpf7aQjuKyFH25xzn3pHn3TVNy01Hl2hyy6YdkSpKsS9sl/6RlI3rRu3dxWd6spwnAGPIJTfl925fcLaoSDHXvyo6i9SlCQrU9wKln3OyWiaDN1RAbW3kKbSd7gPtwMkH/tTWy9afuy1iPfnXMAblITwkE4yf08cn3pSbYt1uts24XH6fUbiLAuY1MWyGkLEmUW0rcCRvUpQ5CtwKQCPgi4S1ZbDe4sd9NntDEe79m3uOBLTr0IR9jzodSMqUpTu9JJ8owD7UTT4ZCfv9PbP7860m+s+HBSrejWRuz2kAxoesGYxTW/Zlpkwo1vkuSly3UgKWQUhHJUvIHsAaKTemF8XE6sWmxonButtonSubmityZkiaZrMh1jv8ArQNpUVqB8FW0njHqx4zRVVhsph1KlKk5xQ+7uHmikaSJrQerMByet2IwvtuTLa4xv2k7Rk84H9x/esHv92d01boenLXGcuiWrFIhLlpbcaQ2/JdK3VJCkAq2pAR7Zz7YxWudY9fxNIdQbNGkR5TyX4aisNNpUMFZAzkj4NK0jq9ZpbLr0PSlzkhrlZDaQlP3P8Q4/ap3F87bPucJEkx/hHv60b2TYXLrKN5sramYECSQRk9M6c6zmJ+eb5Hi22M7cnWGIQgFLbX0zSo4PDa1YBcTgDyMjJ/qbGPabH08SJt1Uzc9QqRliGg5QySPKvgc+TyfYDmmTUWpNYz7ctxoQdPQshCktupckDJUPUcJT6DwMq8YyaQ9VL0pCS8zapcq4SVOBZmPDO8/cnknlWcDBwn4NYnPjLkQ+qE9OtOVlYpeVHDCEkkkJyT+SuQzy5Y0ru6Ez511/Efa5s1fdkOtyVurIxgdlQAA9gOKKPwolU7remU5hCGYEgo38KUv9I/0TRTDYJCWQBSF4rIN/CRgAR0iTpVD1j1g/qDqJcJqlKcjB9bcda142MpOEJAzgeMnjyTSyze5KEuNRpDoDvC0oe4X9iAeaKKFK+oya6fbOqYbDTeEiAPKpHdS3gBLYc7RQkp3ApQog+cq8nwPJrljzxnPZbUfnugn/NFFRgEVch9xKsH0H8pg6e3x3T3UC1ajaZITGkJLoS4MKbOUrzz/ACKVRRRVzVwtoQmhG1NkWu0HuI+JI8u/Kv/Z';
+
+    ///////////////////////////////////////////////////////////////////////////////////
+    // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+    ///////////////////////////////////////////////////////////////////////////////////
+
+    const raw = JSON.stringify({
+      user_app_id: {
+        user_id: USER_ID,
+        app_id: APP_ID,
+      },
+      inputs: [
+        {
+          data: {
+            image: {
+              url: IMAGE_URL,
+              // "base64": IMAGE_BYTES_STRING
+            },
+          },
+        },
+      ],
+    });
+
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        Authorization: 'Key ' + PAT,
+      },
+      body: raw,
+    };
+
+    fetch(
+      'https://api.clarifai.com/v2/models/' + MODEL_ID + '/outputs',
+      requestOptions
+    )
+      .then(response => response.json())
+      .then(result => {
+        console.log('my result: ', result);
+        const regions = result.outputs[0].data.regions;
+        this.displayFaceBox(this.calculateFaceLocation(result));
+
+        regions.forEach(region => {
+          // Accessing and rounding the bounding box values
+          const boundingBox = region.region_info.bounding_box;
+          const topRow = boundingBox.top_row.toFixed(3);
+          const leftCol = boundingBox.left_col.toFixed(3);
+          const bottomRow = boundingBox.bottom_row.toFixed(3);
+          const rightCol = boundingBox.right_col.toFixed(3);
+
+          region.data.concepts.forEach(concept => {
+            // Accessing and rounding the concept value
+            const name = concept.name;
+            const value = concept.value.toFixed(4);
+
+            console.log(
+              `${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol} ${result}`
+            );
+          });
+        });
+      })
+      .catch(error => console.log('error', error));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <ParticlesBg
+          type="cobweb"
+          color="#ff0000"
+          num={200}
+          bg={true}
+          // "color"
+          // "ball"
+          // "lines"
+          // "thick"
+          // "circle"
+          // "cobweb"
+          // "polygon"
+          // "square"
+          // "tadpole"
+          // "fountain"
+          // "random"
+          // "custom"
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+      </div>
+    );
+  }
 }
 
 export default App;
